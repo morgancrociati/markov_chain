72845b109efde17fe1975e61019941d88d1ffd14
Morgan Crociati <cromorg@outlook.com>
2019-12-23 23:28:09 +0100

    markov chain for char and interger types added

---
+++ char/markov_chain_char.hpp
@@ -0,0 +1,40 @@
+#ifndef H_MARKOV_CHAIN_CHAR
+#define H_MARKOV_CHAIN_CHAR
+
+#include <vector>
+#include <string>
+
+#define MARKOV_INT std::size_t
+
+class markov_chain_char
+{
+public:
+
+    markov_chain_char(unsigned int seed = 0);
+    ~markov_chain_char();
+
+    void add_state(char state, MARKOV_INT start = 0);
+    void set_transition(char stateFrom, char stateTo, MARKOV_INT n);
+
+    void add_sample(std::vector<char> tab);
+    void add_sample(std::string tab);
+
+    std::vector<char> generate_vector(MARKOV_INT size);
+    std::string generate_string(MARKOV_INT size);
+
+private:
+    std::vector<std::vector<MARKOV_INT>> matrix;
+    std::vector<MARKOV_INT> sumMatrix;
+
+    std::vector<char> stateNameMatrix;
+    
+    std::vector<MARKOV_INT> startMatrix;
+    MARKOV_INT sumStartMatrix;
+
+    MARKOV_INT find(char element);
+
+    MARKOV_INT get_start(void);
+    char get_next(MARKOV_INT state);
+};
+
+#endif
---
+++ int/markov_chain_int.hpp
@@ -0,0 +1,38 @@
+#ifndef H_MARKOV_CHAIN
+#define H_MARKOV_CHAIN
+
+#include <vector>
+
+#define MARKOV_TYPE int
+#define MARKOV_INT std::size_t
+
+class markov_chain_int
+{
+public:
+
+    markov_chain_int(unsigned int seed = 0);
+    ~markov_chain_int();
+
+    void add_state(MARKOV_TYPE state, MARKOV_INT start = 0);
+    void set_transition(MARKOV_TYPE stateFrom, MARKOV_TYPE stateTo, MARKOV_INT n);
+
+    void add_sample(std::vector<MARKOV_TYPE> tab);
+
+    std::vector<MARKOV_TYPE> generate(MARKOV_INT size);
+
+private:
+    std::vector<std::vector<MARKOV_INT>> matrix;
+    std::vector<MARKOV_INT> sumMatrix;
+
+    std::vector<MARKOV_TYPE> stateNameMatrix;
+    
+    std::vector<MARKOV_INT> startMatrix;
+    MARKOV_INT sumStartMatrix;
+
+    MARKOV_INT find(MARKOV_TYPE element);
+
+    MARKOV_INT get_start(void);
+    MARKOV_TYPE get_next(MARKOV_INT state);
+};
+
+#endif
